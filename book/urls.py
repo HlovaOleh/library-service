746db50b3c7d97456608ad6b988f0db542ba3b10
Hlova Oleh <hlovaoleh0@gmail.com>
2024-02-07 01:33:51 +0200

    implemented book viewset and urls

--- book/urls.py
+++ book/urls.py
@@ -1,5 +1,14 @@
-urlpatterns = [
+from django.urls import include, path
+from rest_framework import routers
 
+from book import views
+
+router = routers.DefaultRouter()
+router.register("books", views.BookViewSet)
+
+
+urlpatterns = [
+    path("", include(router.urls))
 ]
 
 app_name = "book"
