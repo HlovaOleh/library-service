a22451e6a4868a1730d663c7db6b17c0ae81f235
Hlova Oleh <hlovaoleh0@gmail.com>
2024-02-15 22:07:31 +0200

    implemented serializers for borrowing

---
+++ borrowing/serializers.py
@@ -0,0 +1,39 @@
+from django.db import transaction
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
+from borrowing.models import Borrowing
+
+
+class BorrowingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = (
+            "pk",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book",
+            "user",
+            "is_active"
+        )
+
+
+class BorrowingCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = ("book",)
+
+    @transaction.atomic
+    def create(self, validated_data):
+        user = validated_data.get("user")
+        borrowed_book = validated_data.get("book")
+        if borrowed_book.inventory == 0:
+            raise ValidationError("Sorry no books available!")
+        if user.borrowings.filter(is_active=True).count():
+            raise ValidationError("You must return your active borrowing!")
+        borrowed_book.inventory -= 1
+        borrowed_book.save()
+        return Borrowing.objects.create(
+            **validated_data
+        )
