d0ee03cd5f1143e5fbaf22aa8ea999a2b3eeefa8
Hlova Oleh <hlovaoleh0@gmail.com>
2024-03-25 12:05:25 +0200

    created celery app and started working on scheduled tasks

---
+++ borrowing/task.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import, unicode_literals
+
+from celery import shared_task
+from celery.utils.log import get_task_logger
+from django.utils import timezone
+
+from borrowing.models import Borrowing
+
+logger = get_task_logger(__name__)
+
+
+@shared_task(name="check_overdue_task")
+def check_overdue_task(self):
+    borrowings = Borrowing.objects.filter(
+        expected_return_date__lte=timezone.now()+timezone.timedelta(days=1)
+    )
---
+++ library_service/celery.py
@@ -0,0 +1,36 @@
+from __future__ import absolute_import, unicode_literals
+import os
+
+from celery import Celery
+from django.conf import settings
+
+# Set the default Django settings module for the 'celery' program.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "library_service.settings")
+
+app = Celery("library_service")
+app.conf.enable_utc = False
+
+app.conf.update(timezone="Ukraine/Kyiv")
+
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Celery Beat Settings
+# app.conf.beat_schedule = {
+#     'send-mail-every-day-at-8': {
+#         'task': 'send_mail_app.tasks.send_mail_func',
+#         'schedule': crontab(hour=0, minute=46, day_of_month=19, month_of_year = 6),
+#     }
+
+# }
+
+# Load task modules from all registered Django apps.
+app.autodiscover_tasks()
+
+
+@app.task(bind=True, ignore_result=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
