14b1ce2b20533af0121117420f6631a334a0246b
Hlova Oleh <hlovaoleh0@gmail.com>
2024-03-28 18:55:48 +0200

    added new command to manage.py and connect telegram bot

--- notification/bot_commands.py
+++ notification/bot_commands.py
@@ -1,12 +1,5 @@
-import telebot
-from library_service.settings import TELEGRAM_BOT_TOKEN, CHAT_ID
-
-
-bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
-
-
-@bot.message_handler(commands=["start"])
-def send_welcome(message: telebot.types.Message) -> None:
+def welcome_message(bot, message) -> None:
+    name = ""
 
     if message.from_user.last_name is None:
         name = f"{message.from_user.first_name}"
@@ -19,16 +12,8 @@ def send_welcome(message: telebot.types.Message) -> None:
                  f"/help for more information")
 
 
-@bot.message_handler(commands=["help"])
-def send_help_information(message: telebot.types.Message) -> None:
+def help_information(bot, message) -> None:
     bot.reply_to(message,
                  "/start - start bot\n"
                  "/help - show information about commands\n"
                  "/my_borrowings - show all your borrowings\n")
-
-
-def send_telegram_notification(message):
-    bot.send_message(CHAT_ID, text=message)
-
-
-bot.polling()
